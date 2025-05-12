d4c3a2efadddd38f9fc283e4823cdccf12a161cd
DesryLoic <loic.desry@student.junia.com>
2025-05-12 17:34:18 +0200

    menu pause

---
+++ Scènes/UIDs/PauseMenu.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class PauseMenu : Control
+{
+    private Button _resumeButton;
+    private Button _quitButton;
+
+    public override void _Ready()
+    {
+        _resumeButton = GetNode<Button>("screenPanel/screenVBox/ResumeButton");
+        _quitButton = GetNode<Button>("screenPanel/screenVBox/QuitButton");
+
+        _resumeButton.Pressed += OnResumePressed;
+        _quitButton.Pressed += OnQuitPressed;
+
+        Hide(); // Cache le menu au début
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("ui_cancel"))
+        {
+            var canvasLayer = GetParent(); // PauseMenu est enfant de CanvasLayer
+            var deadScreenNode = canvasLayer.GetNodeOrNull<DeadScreen>("DeadScreen");
+
+            if (deadScreenNode != null && !deadScreenNode.IsDeathScreenVisible())
+            {
+                if (Visible)
+                {
+                    GetTree().Paused = false;
+                    Hide();
+                }
+                else
+                {
+                    GetTree().Paused = true;
+                    Show();
+                }
+            }
+        }
+    }
+
+
+    private void OnResumePressed()
+    {
+        GetTree().Paused = false;
+        Hide();
+    }
+
+    private void OnQuitPressed()
+    {
+        GetTree().Quit();
+    }
+}

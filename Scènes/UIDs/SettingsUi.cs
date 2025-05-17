684c1ce508a1558902381fd0596277d24be5ba7f
SansStylo <kylian.opel@student.junia.com>
2025-05-17 17:05:33 +0200

    fix mapping Ui

--- Scènes/UIDs/SettingsUi.cs
+++ Scènes/UIDs/SettingsUi.cs
@@ -22,56 +22,67 @@ public partial class SettingsUi : Panel
         // Remap du bouton "Regarder en haut"
         Button lookUpButton = GetNode<Button>("lookUpButton");
         RichTextLabel lookUpLabel = GetNode<RichTextLabel>("lookUpButton/inputLabel");
+        lookUpButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[lookUpButton] = new RemapData { ActionName = "z", Label = lookUpLabel };
 
         // Remap du bouton "Regarder en bas"
         Button lookDownButton = GetNode<Button>("lookDownButton");
         RichTextLabel lookDownLabel = GetNode<RichTextLabel>("lookDownButton/inputLabel");
+        lookDownButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[lookDownButton] = new RemapData { ActionName = "s", Label = lookDownLabel };
 
         // Remap du bouton "Aller à gauche"
         Button goLeftButton = GetNode<Button>("goLeftButton");
         RichTextLabel goLeftLabel = GetNode<RichTextLabel>("goLeftButton/inputLabel");
+        goLeftButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[goLeftButton] = new RemapData { ActionName = "q", Label = goLeftLabel };
 
         // Remap du bouton "Aller à droite"
         Button goRightButton = GetNode<Button>("goRightButton");
         RichTextLabel goRightLabel = GetNode<RichTextLabel>("goRightButton/inputLabel");
+        goRightButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[goRightButton] = new RemapData { ActionName = "d", Label = goRightLabel };
 
         // Remap du bouton "Sauter"
         Button jumpButton = GetNode<Button>("jumpButton");
         RichTextLabel jumpLabel = GetNode<RichTextLabel>("jumpButton/inputLabel");
+        jumpButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[jumpButton] = new RemapData { ActionName = "jump", Label = jumpLabel };
 
         // Remap du bouton "Attaque"
         Button atkButton = GetNode<Button>("atkButton");
         RichTextLabel atkLabel = GetNode<RichTextLabel>("atkButton/inputLabel");
+        atkButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[atkButton] = new RemapData { ActionName = "atk", Label = atkLabel };
 
         // Remap du bouton "Attaque secondaire"
         Button atkSecButton = GetNode<Button>("atkSecButton");
         RichTextLabel atkSecLabel = GetNode<RichTextLabel>("atkSecButton/inputLabel");
+        atkSecButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[atkSecButton] = new RemapData { ActionName = "atk_sec", Label = atkSecLabel };
 
         // Remap du bouton "Attaque tertiaire"
         Button atkTerButton = GetNode<Button>("atkTerButton");
         RichTextLabel atkTerLabel = GetNode<RichTextLabel>("atkTerButton/inputLabel");
+        atkTerButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[atkTerButton] = new RemapData { ActionName = "atk_ter", Label = atkTerLabel };
 
         // Remap du bouton "Attaque quaternaire"
         Button atkQuaButton = GetNode<Button>("atkQuaButton");
         RichTextLabel atkQuaLabel = GetNode<RichTextLabel>("atkQuaButton/inputLabel");
+        atkQuaButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[atkQuaButton] = new RemapData { ActionName = "atk_qua", Label = atkQuaLabel };
 
         // Remap du bouton "Inventaire"
         Button invButton = GetNode<Button>("invButton");
         RichTextLabel invLabel = GetNode<RichTextLabel>("invButton/inputLabel");
+        invButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[invButton] = new RemapData { ActionName = "toggle_inventory_gui", Label = invLabel };
 
         // Remap du bouton "Absorption"
         Button absButton = GetNode<Button>("absButton");
         RichTextLabel absLabel = GetNode<RichTextLabel>("absButton/inputLabel");
+        absButton.FocusMode = Control.FocusModeEnum.None;
         remapBindings[absButton] = new RemapData { ActionName = "abs", Label = absLabel };
 
         // Lier tous les boutons à un seul handler
@@ -95,15 +106,21 @@ public partial class SettingsUi : Panel
         if (!waitingForInput || currentRemap == null)
             return;
 
-        if (@event is InputEventKey keyEvent && keyEvent.Pressed)
+        else if (@event is InputEventKey keyEvent)
         {
-            InputMap.ActionEraseEvents(currentRemap.ActionName);
-            InputMap.ActionAddEvent(currentRemap.ActionName, keyEvent);
-            currentRemap.Label.Text = OS.GetKeycodeString(keyEvent.Keycode);
+            if (keyEvent.Pressed && !keyEvent.Echo)
+            {
+                InputMap.ActionEraseEvents(currentRemap.ActionName);
+                InputMap.ActionAddEvent(currentRemap.ActionName, keyEvent);
+                currentRemap.Label.Text = OS.GetKeycodeString(keyEvent.Keycode);
 
-            // Reset
-            currentRemap = null;
-            waitingForInput = false;
+                currentRemap = null;
+                waitingForInput = false;
+            }
+            else
+            {
+                GetViewport().SetInputAsHandled();
+            }
         }
     }
 }

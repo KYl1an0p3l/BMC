0491d9903ceae47999f4d9d496c4e71f9725869e
DesryLoic <loic.desry@student.junia.com>
2025-05-19 15:07:41 +0200

    boss démo 1

--- Scènes/Enemies/Boss.cs
+++ Scènes/Enemies/Boss.cs
@@ -1,36 +1,26 @@
 using Godot;
 using System;
 
-public partial class Enemy1 : CharacterBody2D
+public partial class Boss : CharacterBody2D
 {
     [Export] public int Health = 6;
     [Export] public float Speed = 100f;
     [Export] public float Gravity = 800f;
-    [Export] public float MaxFallSpeed = 200f;
+    [Export] public float MaxFallSpeed = 400f;
 
     private Vector2 _velocity = Vector2.Zero;
     private Vector2 direction = Vector2.Left;
-    private AnimatedSprite2D Sprite;    
-    private AudioStreamPlayer2D[] Sounds;
+    private AnimatedSprite2D Sprite;
     private RayCast2D rayLeft;
     private RayCast2D rayRight;
 
     private Pp overlappingPlayer = null;
 
-    // Nouveau : gestion du mouvement/arrêt
     private bool isMoving = true;
     private float stateTimer = 0f;
-    private const float StateDuration = 1f;
 
     public override void _Ready()
     {
-        GD.Randomize();
-        Sounds = new AudioStreamPlayer2D[]
-        {
-            GetNode<AudioStreamPlayer2D>("Sound1"),
-            GetNode<AudioStreamPlayer2D>("Sound2"),
-            GetNode<AudioStreamPlayer2D>("Sound3")
-        };
         Sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         rayLeft = GetNode<RayCast2D>("RayLeft");
         rayRight = GetNode<RayCast2D>("RayRight");
@@ -50,15 +40,6 @@ public partial class Enemy1 : CharacterBody2D
             overlappingPlayer.TakeDamage(1);
         }
 
-        stateTimer += (float)delta;
-        if (stateTimer >= StateDuration)
-        {
-            isMoving = !isMoving;
-            stateTimer = 0f;
-
-            if (isMoving)
-                PlayRandomSound();
-        }
     }
 
     public override void _PhysicsProcess(double delta)
@@ -71,13 +52,6 @@ public partial class Enemy1 : CharacterBody2D
                 Sprite.Play("left");
             else if (direction == Vector2.Right)
                 Sprite.Play("right");
-        }
-        else
-        {
-            if (direction == Vector2.Left)
-                Sprite.Play("idle_left");
-            else if (direction == Vector2.Right)
-                Sprite.Play("idle_right");
         }
 
         // Gravité
@@ -100,11 +74,6 @@ public partial class Enemy1 : CharacterBody2D
             else if (direction == Vector2.Right && !rayRight.IsColliding())
                 direction = Vector2.Left;
         }
-    }
-        private void PlayRandomSound()
-    {
-        int index = (int)(GD.Randi() % (ulong)Sounds.Length);
-        Sounds[index].Play();
     }
     private void OnBodyEntered(Node body)
     {
